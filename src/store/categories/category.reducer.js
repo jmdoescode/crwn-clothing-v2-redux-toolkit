0d9f66d71a0792b75aa899fae40f745dca7db763
jm codes <jmdoescode@gmail.com>
2023-07-17 22:33:38 -0700

    Revert "188. Categories to createSlice"
    This reverts commit 3a5d79e7075171096603db738713c56bae7505ff.

--- src/store/categories/category.reducer.js
+++ src/store/categories/category.reducer.js
@@ -1,34 +1,19 @@
-import { createSlice } from "@reduxjs/toolkit";
-// import { CATEGORIES_ACTION_TYPES } from './category.types';
+import { CATEGORIES_ACTION_TYPES } from './category.types';
 
 export const CATEGORIES_INITIAL_STATE = {
-	categories: [],
+  categories: [],
 };
 
-export const categoriesSlice = createSlice({
-	name: "categories",
-	initialState: CATEGORIES_INITIAL_STATE,
-	reducers: {
-		setCategories(state, action) {
-			state.categories = action.payload;
-		},
-	},
-});
+export const categoriesReducer = (
+  state = CATEGORIES_INITIAL_STATE,
+  action = {}
+) => {
+  const { type, payload } = action;
 
-export const { setCategories } = categoriesSlice.actions;
-
-export const categoriesReducer = categoriesSlice.reducer;
-
-// export const categoriesReducerOld = (
-//   state = CATEGORIES_INITIAL_STATE,
-//   action = {}
-// ) => {
-//   const { type, payload } = action;
-
-//   switch (type) {
-//     case CATEGORIES_ACTION_TYPES.SET_CATEGORIES:
-//       return { ...state, categories: payload };
-//     default:
-//       return state;
-//   }
-// };
+  switch (type) {
+    case CATEGORIES_ACTION_TYPES.SET_CATEGORIES:
+      return { ...state, categories: payload };
+    default:
+      return state;
+  }
+};
