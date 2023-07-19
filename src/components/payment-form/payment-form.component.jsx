357c5716f66a82160867aab71454604ab2dff315
jmdoescode <jmdoescode@gmail.com>
2023-07-18 20:19:55 -0700

    193. Setting up our PaymentForm - pt 2

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -1,14 +1,28 @@
-import { CardElement } from "@stripe/react-stripe-js";
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 
 import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
 
 import { PaymentsFormContainer, FormContainer } from "./payment-form.styles";
 
 export const PaymentForm = () => {
+	const stripe = useStripe();
+	const elements = useElements();
+
+	const paymentHandler = async (e) => {
+		e.preventDefault();
+
+		if (!stripe || !elements) {
+			return;
+		}
+
+    //create payment intent
+    
+	};
+
 	return (
 		<PaymentsFormContainer>
 			<FormContainer>
-      <h2>Credit Card Payment: </h2>
+				<h2>Credit Card Payment: </h2>
 				<CardElement />
 				<Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay Now</Button>
 			</FormContainer>
