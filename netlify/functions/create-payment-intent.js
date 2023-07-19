c23f455ef116241f659dcfa5e5b7f4909225bc90
jmdoescode <jmdoescode@gmail.com>
2023-07-18 23:02:33 -0700

    195. Finishing Strip Payment - GETTING ERROR

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -1,5 +1,7 @@
 require("dotenv").config(); //require is an old version of import
-const stripe = require("stripe")(`${process.env.STRIPE_SECRET_KEY}`);
+const stripe = require("stripe")("sk_test_51NV7vuBokOBEeErhg7HHHbRq1XLC8lmfgRQ7dF1nOBmPMEObJ4kxZnjpjWMVNL8jh5UR7WFFt3zBKbEdi9m78Ool00G1kauhp9");
+//console.log('STRIPE_SECRET_KEY', process.env.STRIPE_SECRET_KEY);
+//const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 exports.handler = async (event) => {
 	try {
--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -1,4 +1,9 @@
-import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import {
+	CardElement,
+	useStripe,
+	useElements,
+	setClientSecret,
+} from "@stripe/react-stripe-js";
 
 import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
 
@@ -15,15 +20,56 @@ export const PaymentForm = () => {
 			return;
 		}
 
-    //create payment intent
-    
+		//create payment intent
+		//route relative to your url
+		const response = await fetch("/.netlify/functions/create-payment-intent", {
+			method: "post",
+			headers: {
+				"Content-Type": "application/json",
+				"Access-Control-Allow-Origin": "*", //Added bc getting error: "blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requeseted resource
+			},
+			body: JSON.stringify({ amount: 10000 }),
+		})
+			.then((res) => res.json())
+			.catch((err) => console.log(err));
+
+		console.log("response", response);
+
+		// const { paymentIntent } = response;
+		// console.log('paymentIntent', paymentIntent);
+
+		//const clientSecret = response.payementIntent.client_secret; //other way to get clientSecret
+		const {
+			payementIntent: { client_secret }, //make sure payment is payEment
+		} = response;
+		// console.log('client_secret', client_secret);
+
+		const paymentResult = await stripe.confirmCardPayment(client_secret, {
+			payment_method: {
+				card: elements.getElement(CardElement),
+				billing_details: {
+					name: "Test User",
+				},
+			},
+		});
+
+		console.log('paymentResult', paymentResult);
+
+		if (paymentResult.error) {
+			alert(paymentResult.error);
+		} else {
+			if (paymentResult.payementIntent.status === "succeeded") {
+				alert("Payment Suceeded");
+			}
+		}
 	};
 
 	return (
 		<PaymentsFormContainer>
-			<FormContainer>
+			<FormContainer onSubmit={paymentHandler}>
 				<h2>Credit Card Payment: </h2>
 				<CardElement />
+				{/*Any button inside the form container will automatically be a submission button*/}
 				<Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay Now</Button>
 			</FormContainer>
 		</PaymentsFormContainer>
